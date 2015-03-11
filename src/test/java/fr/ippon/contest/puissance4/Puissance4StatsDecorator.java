91add3db0d3c567f1c6603d0eb06aa31096116a0
Nicolas GUIBERT <nguibert@ippon.fr>
2015-03-11 17:24:36 +0100

    nettoyage du décorateur

--- src/test/java/fr/ippon/contest/puissance4/Puissance4StatsDecorator.java
+++ src/test/java/fr/ippon/contest/puissance4/Puissance4StatsDecorator.java
@@ -14,10 +14,10 @@ public class Puissance4StatsDecorator implements Puissance4 {
 	 */
 	private final String NOUVEAU_JEU = "nouveauJeu";
 	private final String CHARGER_JEU = "chargerJeu";
-	private final String ETAT_JEU = "getEtatJeu";
-	private final String TOUR = "getTour";
-	private final String OCCUPANT = "getOccupant";
-	private final String JOUER = "jouer";
+	private final String ETAT_JEU 	 = "getEtatJeu";
+	private final String TOUR 		 = "getTour";
+	private final String OCCUPANT 	 = "getOccupant";
+	private final String JOUER 		 = "jouer";
 
 	/**
 	 * Instance à décorer
@@ -27,102 +27,69 @@ public class Puissance4StatsDecorator implements Puissance4 {
 	 * Map stockant les temps d'exécution pour chaque méthode de l'API.
 	 */
 	private Map<String, List<Long>> statsExec;
-	
+
 	public Puissance4StatsDecorator(Puissance4 jeuADecorer) {
 		jeu = jeuADecorer;
 		statsExec = new HashMap<String, List<Long>>();
 	}
 
 	@Override
-	public void nouveauJeu() {
+	public synchronized void nouveauJeu() {
 		long top = System.nanoTime();
 		jeu.nouveauJeu();
-		synchronized (statsExec) {
-			if (statsExec.containsKey(NOUVEAU_JEU)) {
-				statsExec.get(NOUVEAU_JEU).add(System.nanoTime() - top);
-			} else {
-				List<Long> execTimes = new ArrayList<Long>();
-				execTimes.add(System.nanoTime() - top);
-				statsExec.put(NOUVEAU_JEU, execTimes);
-			}
-		}
+		collectStats(NOUVEAU_JEU, top);
 	}
 
 	@Override
-	public void chargerJeu(char[][] grille, char tour) {
+	public synchronized void chargerJeu(char[][] grille, char tour) {
 		long top = System.nanoTime();
 		jeu.chargerJeu(grille, tour);
-		synchronized (statsExec) {
-			if (statsExec.containsKey(CHARGER_JEU)) {
-				statsExec.get(CHARGER_JEU).add(System.nanoTime() - top);
-			} else {
-				List<Long> execTimes = new ArrayList<Long>();
-				execTimes.add(System.nanoTime() - top);
-				statsExec.put(CHARGER_JEU, execTimes);
-			}
-		}
+		collectStats(CHARGER_JEU, top);
 	}
 
 	@Override
-	public EtatJeu getEtatJeu() {
+	public synchronized EtatJeu getEtatJeu() {
 		long top = System.nanoTime();
 		EtatJeu etat = jeu.getEtatJeu();
-		synchronized (statsExec) {
-			if (statsExec.containsKey(ETAT_JEU)) {
-				statsExec.get(ETAT_JEU).add(System.nanoTime() - top);
-			} else {
-				List<Long> execTimes = new ArrayList<Long>();
-				execTimes.add(System.nanoTime() - top);
-				statsExec.put(ETAT_JEU, execTimes);
-			}
-		}
+		collectStats(ETAT_JEU, top);
 		return etat;
 	}
 
 	@Override
-	public char getTour() {
+	public synchronized char getTour() {
 		long top = System.nanoTime();
 		char tour = jeu.getTour();
-		synchronized (statsExec) {
-			if (statsExec.containsKey(TOUR)) {
-				statsExec.get(TOUR).add(System.nanoTime() - top);
-			} else {
-				List<Long> execTimes = new ArrayList<Long>();
-				execTimes.add(System.nanoTime() - top);
-				statsExec.put(TOUR, execTimes);
-			}
-		}
+		collectStats(TOUR, top);
 		return tour;
 	}
 
 	@Override
-	public char getOccupant(int ligne, int colonne) {
+	public synchronized char getOccupant(int ligne, int colonne) {
 		long top = System.nanoTime();
 		char occupant = jeu.getOccupant(ligne, colonne);
-		synchronized (statsExec) {
-			if (statsExec.containsKey(OCCUPANT)) {
-				statsExec.get(OCCUPANT).add(System.nanoTime() - top);
-			} else {
-				List<Long> execTimes = new ArrayList<Long>();
-				execTimes.add(System.nanoTime() - top);
-				statsExec.put(OCCUPANT, execTimes);
-			}
-		}
+		collectStats(OCCUPANT, top);
 		return occupant;
 	}
 
 	@Override
-	public void jouer(int colonne) {
+	public synchronized void jouer(int colonne) {
 		long top = System.nanoTime();
 		jeu.jouer(colonne);
-		synchronized (statsExec) {
-			if (statsExec.containsKey(JOUER)) {
-				statsExec.get(JOUER).add(System.nanoTime() - top);
-			} else {
-				List<Long> execTimes = new ArrayList<Long>();
-				execTimes.add(System.nanoTime() - top);
-				statsExec.put(JOUER, execTimes);
-			}
+		collectStats(JOUER, top);
+	}
+	/**
+	 * Fonction appellée pour la maj des stats d'une méthode
+	 * 
+	 * @param methodName nom de la méthode qui a été appelée
+	 * @param top		 top donné avant l'appel par System.nanoTime()
+	 */
+	private void collectStats(String methodName, long top) {
+		if (statsExec.containsKey(methodName)) {
+			statsExec.get(methodName).add(System.nanoTime() - top);
+		} else {
+			List<Long> execTimes = new ArrayList<Long>();
+			execTimes.add(System.nanoTime() - top);
+			statsExec.put(methodName, execTimes);
 		}
 	}
 
@@ -136,7 +103,4 @@ public class Puissance4StatsDecorator implements Puissance4 {
 			}
 		}
 	}
-
-	
-	
 }
